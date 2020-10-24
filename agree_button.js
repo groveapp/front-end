19721300e898011aca228d60675523e3d1e3f8e8
amanjpatel <60860107+amanjpatel@users.noreply.github.com>
2020-10-23 21:26:28 -0400

    Update agree_button.js

--- agree_button.js
+++ agree_button.js
@@ -1,1 +1,25 @@
+'use strict';
 
+const e = React.createElement;
+
+class AgreeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
+
+  render() {
+    if (this.state.clicked) {
+      return 'Added one to the counter:';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ clicked: true }) },
+      'Agree with this statement'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#Agree_button');
+ReactDOM.render(e(LikeButton), domContainer);
