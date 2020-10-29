e14e00d3f0418c8da48e265c747f057ce64f9867
amanjpatel <amanpatel1777@gmail.com>
2020-10-28 20:24:17 -0400

    Did a simple app

--- accountability/lib/main.dart
+++ accountability/lib/main.dart
@@ -1,113 +1,122 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // #docregion build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Dashboard',
+      routes: {
+        "/": (context) => SignUpScreen(),
+        "/welcome": (context) => DashboardScreen(),
+      },
     );
+    // #enddocregion build
   }
 }
+// #enddocregion MyApp
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+Widget fillColumns(String title) {
+  return Column(children: [
+    Text(title),
+    Text("add info here"),
+    TextButton(
+        onPressed: () {
+          _buttonPress();
+        },
+        child: Text('Hello'))
+  ]);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// #docregion _buildColumns
+Widget _buildColumns() {
+  return //Container(
+      //padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),child:
+      Center(
+          child: Row(children: [
+    Container(
+        padding: EdgeInsets.symmetric(horizontal: 100, vertical: 16),
+        color: Colors.amber,
+        child: Column(children: [fillColumns("New Politician Activity")])),
+    Container(
+        padding: EdgeInsets.symmetric(horizontal: 100, vertical: 16),
+        color: Colors.amber,
+        child: Column(children: [fillColumns("New Issues")])),
+    Container(
+        padding: EdgeInsets.symmetric(horizontal: 100, vertical: 16),
+        color: Colors.amber,
+        child: Column(children: [fillColumns("Followed Page Activity")]))
+  ])); //);
+}
+// #enddocregion _buildColumns
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+// #docregion RWS-build
+class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          leading: Icon(Icons.donut_large_sharp),
+          title: Text('Dashboard'),
+          actions: [
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Text('Browse Politicians')),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Text('Browse Issues')),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Text('Trending')),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.search)),
+            Icon(Icons.person)
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          backgroundColor: Colors.purple),
+      body: _buildColumns(),
+    );
+  }
+// #enddocregion RWS-build
+}
+
+void _buttonPress() {}
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: Icon(Icons.donut_large_sharp),
+          title: Text('Sign Up'),
+          actions: [
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Text('Browse Politicians')),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Text('Browse Issues')),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Text('Trending')),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.search)),
+            Icon(Icons.person)
+          ],
+          backgroundColor: Colors.purple),
+      body: Center(
+          child: TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DashboardScreen()),
+                );
+              },
+              child: Text("Press for Home Screen"))),
     );
   }
 }
